452bb24195c3df46a287e54660d11907b89a4534
Bhavin <55060711+bhavin-rb@users.noreply.github.com>
2024-01-06 11:53:52 +0300

    Update main.jsx
    exact path

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-        <Route path="*" element={ <App /> }>
+        <Route exact path="*" element={ <App /> }>
         </Route>
       </Routes>
     </BrowserRouter>
