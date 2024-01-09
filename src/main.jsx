4541b01b8f2ddd72c6281016e40435d6159b3f5d
Bhavin Borkhataria <bhavin1234@gmail.com>
2024-01-09 22:34:28 +0300

    modified files

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter,  Routes, Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/algebra1_quiz">
-    <Routes>
+    <BrowserRouter>
+    {/* <Routes>
         <Route path="/algebra1_quiz" element={ <App /> }>
         </Route>
-      </Routes>
+      </Routes> */}
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
